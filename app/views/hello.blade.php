81f6b3262f218a82e869e826a0e28661302c17e9
Christopher Schmitz <schmitz.chris@gmail.com>
2014-12-28 20:40:15 -0600

    Removes unneeded style elements from the hello view. Organizes html elements in the body of the hello view

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -3,36 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Laravel PHP Framework</title>
-	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
-
-		body {
-			margin:0;
-			font-family:'Lato', sans-serif;
-			text-align:center;
-			color: #999;
-		}
-
-		.welcome {
-			width: 300px;
-			height: 200px;
-			position: absolute;
-			left: 50%;
-			top: 50%;
-			margin-left: -150px;
-			margin-top: -100px;
-		}
-
-		a, a:visited {
-			text-decoration:none;
-		}
-
-		h1 {
-			font-size: 32px;
-			margin: 16px 0 0 0;
-		}
-	</style>
     <script src="https://code.jquery.com/jquery-2.1.3.min.js" type="text/javascript"></script>
+
     <script>
     $(document).ready(function (){
         $('#getbutton').click( function (){
@@ -48,10 +20,13 @@
     </script>
 </head>
 <body>
-	<div class="welcome">
-        <a href="{{URL::route('tester')}}">checkout the url generated for this link</a>
-        <br>
-        <button id='getbutton'>Click this to see the ajax call work</button>
-	</div>
+    <ul>
+        <li>
+            <a href="{{URL::route('tester')}}">checkout the url generated for this link</a>
+        </li>
+        <li>
+            <button id='getbutton'>Click this to see the ajax call work</button>
+        </li>
+    </ul>
 </body>
 </html>
